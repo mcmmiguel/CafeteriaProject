04c550ec90d3b96f3aa24cbf53b69924d024c4e7
Miguel Cobian <miguelcobian99@gmail.com>
2023-02-21 12:55:43 -0600

    Se añadió todo lo necesario para compilar archivos de SASS con Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
+const { src, dest} = require('gulp');   //Las llaves después de const es porque este gulp exporta multiples funciones
+// cuando no las tenga es porque solo exporta una funcion va. 
+//La funcion src encuentra un archivo y la funcion dest te permite guardarlo en disco va.
 
-function tarea( done ) {
-    console.log ('Desde mi primer tarea...');
+const sass = require('gulp-sass')(require('sass')); //gulp-sass solo compila esa hoja de estilos
+
+function css(  done ) {
+    // Compilar pasos
+    // Paso 1: Identificar archivo, 2. Compilarla, 3. Guardar el .css
+
+    src('src/scss/app.scss')
+        .pipe( sass() )
+            .pipe( dest('build/css') )
 
     done();
 }
 
-exports.tarea = tarea;
+exports.css = css; 
