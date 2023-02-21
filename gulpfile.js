1b87242aea541903731d92fd8cc348f794a141b3
Miguel Cobian <miguelcobian99@gmail.com>
2023-02-21 17:38:13 -0600

    archivos de scss separados. Se añadio modificacion en package para escuchar cambios de toda la carpeta scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,31 @@
-const { src, dest, watch} = require('gulp');   //Las llaves después de const es porque este gulp exporta multiples funciones
+const { src, dest, watch, series} = require('gulp');   //Las llaves después de const es porque este gulp exporta multiples funciones
 // cuando no las tenga es porque solo exporta una funcion va. 
 //La funcion src encuentra un archivo y la funcion dest te permite guardarlo en disco va.
 
 const sass = require('gulp-sass')(require('sass')); //gulp-sass solo compila esa hoja de estilos
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
 
 function css(  done ) {
     // Compilar pasos
     // Paso 1: Identificar archivo, 2. Compilarla, 3. Guardar el .css
 
     src('src/scss/app.scss')
-        .pipe( sass(  {outputStyle: 'expanded'}  ) )
+        .pipe( sass(  ) )
+            .pipe(postcss([autoprefixer () ] ) )  //Para crear codigo que será soportado por otros navegadores
             .pipe( dest('build/css') )
 
     done();
 }
 
 function dev() {
-    watch( 'src/scss/app.scss', css ); 
+    watch( 'src/scss/**/*.scss', css )  //Para tener monitoreada toda la carpeta
 }
 
 exports.css = css; 
 exports.dev = dev;
+exports.default = series(  css, dev );
+
+
+// Series - Se inicia una tarea y hasta que finaliza inicia la siguiente
+// Parallel - Todas inician al mismo tiempo 
