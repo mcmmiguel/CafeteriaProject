a3cfbb67d335d1e798a2b657f9c83ed45f707f9d
Miguel Cobian <miguelcobian99@gmail.com>
2023-02-21 20:31:02 -0600

    Imagenes webp y avif agregadas al build

---
+++ ApuntesDelProyecto.md
@@ -0,0 +1,9 @@
+VER GULPFILE.JS
+
+
+npm i --save-dev gulp-imagemin *Comando para instalar dependencia de gulp para reducir imagenes*
+
+npm i --save-dev gulp-webp *Dependencia para convertir y crear imagenes a formato webp*
+
+npm i --save-dev gulp-avif *Dependencia para convertir y crear imagenes a fomrato avif*
+
--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,16 @@ const { src, dest, watch, series} = require('gulp');   //Las llaves después de 
 // cuando no las tenga es porque solo exporta una funcion va. 
 //La funcion src encuentra un archivo y la funcion dest te permite guardarlo en disco va.
 
+
+// GRUPO CSS Y SASS
 const sass = require('gulp-sass')(require('sass')); //gulp-sass solo compila esa hoja de estilos
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
+
+//GRUPO IMAGENES 
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(  done ) {
     // Compilar pasos
@@ -20,7 +27,26 @@ function css(  done ) {
 
 function imagenes() {
     return src( 'src/img/**/*' )
-        .pipe( dest( 'build/img' ) );
+        .pipe(  imagemin({  optimizationLevel: 3 })  )
+            .pipe( dest( 'build/img' ) );
+}
+
+function imagenWebp() {
+    const opciones = {
+        quality: 50
+    }
+    return src( 'src/img/**/*.{png,jpg}' ) //Solo lo hará con imagenes png y jpg
+        .pipe(webp(opciones) )
+            .pipe( dest('build/img') );
+}
+
+function imagenAvif() {
+    const opciones = {
+        quality: 50
+    }
+    return src( 'src/img/**/*.{png,jpg}' )
+    .pipe(avif(opciones) )
+        .pipe(dest( 'build/img' ));
 }
 
 function dev() {
@@ -33,7 +59,9 @@ function dev() {
 exports.css = css; 
 exports.dev = dev;
 exports.imagenes = imagenes;
-exports.default = series(  imagenes, css, dev );
+exports.imagenWebp = imagenWebp;
+exports.imagenAvif =imagenAvif;
+exports.default = series(  imagenes, imagenWebp, imagenAvif, css, dev );
 
 
 // Series - Se inicia una tarea y hasta que finaliza inicia la siguiente
